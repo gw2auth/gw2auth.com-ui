7da3f4e591451087d00a8db7e160d4828ae8d8e9
Felix <23635466+its-felix@users.noreply.github.com>
2023-08-18 00:43:54 +0200

    feat: update SotO banner

--- angular/src/app/banner/banner.component.ts
+++ angular/src/app/banner/banner.component.ts
@@ -14,17 +14,21 @@ export class BannerComponent implements AfterViewInit {
   showApiIssuesBanner: boolean;
   showSotoReleaseBanner: boolean;
   sotoReleaseDate: string;
+  sotoApiDowntimeFromDate: string;
+  sotoApiDowntimeUntilDate: string;
 
   private offcanvasRef?: NgbOffcanvasRef
 
   constructor(private readonly offcanvasService: NgbOffcanvas, private readonly httpClient: HttpClient) {
     const showSotoFrom = Date.parse('2023-08-18T00:00:00.000Z');
-    const showSotoUntil = Date.parse('2023-08-29T00:00:00.000Z');
+    const showSotoUntil = Date.parse('2023-08-25T00:00:00.000Z');
     const now = Date.now();
 
     this.showApiIssuesBanner = false;
     this.showSotoReleaseBanner = now >= showSotoFrom && now <= showSotoUntil;
-    this.sotoReleaseDate = new Date('2023-08-22T16:00:00.000Z').toLocaleString();
+    this.sotoReleaseDate = new Date('2023-08-22T16:00:00.000Z').toLocaleString();// seattle 9am
+    this.sotoApiDowntimeFromDate = new Date('2023-08-18T07:00:00.000Z').toLocaleDateString();// seattle 00:00
+    this.sotoApiDowntimeUntilDate = new Date('2023-08-24T06:59:59.999Z').toLocaleDateString();// seattle 23:59
   }
 
   ngAfterViewInit(): void {
